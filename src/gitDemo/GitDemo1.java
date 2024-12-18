4a0d61e5fa7e133a24e0caba4341680a46c04353
lahupande01 <lahupande01@gmail.com>
2024-12-18 09:05:34 +0530

    revoking chanhes

--- src/gitDemo/GitDemo1.java
+++
@@ -1,9 +0,0 @@
-package gitDemo;
-
-public class GitDemo1 {
-	
-	public static void main(String[] args) {
-		System.out.println("hello git");
-	}
-
-}
--- src/gitDemo/GitDemo2.java
+++
@@ -1,9 +0,0 @@
-package gitDemo;
-
-public class GitDemo2 {
-	private void sysout() {
-		// TODO Auto-generated method stub
-
-	}
-
-}
